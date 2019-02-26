16b4882631fd64580043c1ed57e763f2a43cfd0d
Philipp Thaler <thalerphilipp1@gmail.com>
2019-02-26 20:56:25 +0100

    Changed Java version to 1.8

--- src/main/java/com/akif/tablereservation/App.java
+++ src/main/java/com/akif/tablereservation/App.java
@@ -1,13 +1,21 @@
 package com.akif.tablereservation;
 
+import com.akif.tablereservation.presentation.view.Menu;
+import com.akif.tablereservation.presentation.view.TableReservationMenu;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App implements startApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    @Override
+    public void start() {
+        Menu menu = new TableReservationMenu();
+        while (true) {
+            menu.showMenu();
+        }
     }
 }
